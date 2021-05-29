496e5b2b415c753814fc5c2398cafb9f19799fad
kjw <gooeby@gmail.com>
2021-05-30 01:45:07 +0900

    change image when press a button

--- 3_Dicee-iOS13/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 3_Dicee-iOS13/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -14,20 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftSideImage = 1
+    var rightSideImage = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //WHO.WHAT = VALUE
-        //VALUE:imageliteral == directly select an image
-        diceImageView1.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceThree")
         // Do any additional setup after loading the view.
     }
 
     @IBAction func diceRollButtonPressed(_ sender: UIButton) {
-        print("Button got pressed.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftSideImage]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightSideImage]
+        
+        leftSideImage += 1
+        rightSideImage -= 1
     }
     
 }
