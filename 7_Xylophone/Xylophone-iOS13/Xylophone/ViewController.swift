46a9812bd09dccc747884f33bf5ee870095306fe
kjw <gooeby@gmail.com>
2021-06-10 03:22:27 +0900

    xylophone reupload

---
+++ 7_Xylophone/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        // sender.currentTitle은 자료형이 String이 아닌 String? 이다. (optional string)
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
