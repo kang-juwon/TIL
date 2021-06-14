d2a2fedc59c74c0ddb04e1b6595cf8ddc2ed47c0
kjw <gooeby@gmail.com>
2021-06-14 23:21:47 +0900

    egg timer - add timer func

--- 8_EggTimer/EggTimer-iOS13/EggTimer/ViewController.swift
+++ 8_EggTimer/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -12,16 +12,30 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = [
-        "Soft" : 5,
-        "Medium" : 7,
-        "Hard" : 12
+        "Soft" : 300,
+        "Medium" : 420,
+        "Hard" : 720
     ]
+    
+    var counter : Int?
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let hardness = sender.currentTitle!
         
         print(eggTimes[hardness]!)
-    }
+        
+        counter = eggTimes[sender.currentTitle!]
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
+        }
     
-}
+    @objc func updateCounter() {
+        if counter! > 0 {
+            print("\(String(describing: counter)) seconds")
+            counter! -= 1
+        }
+    }
+
+
+    }
