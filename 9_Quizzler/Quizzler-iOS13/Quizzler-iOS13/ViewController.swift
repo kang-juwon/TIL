53a3c10a075277ea794855d9e86ea77257a2c143
kjw <gooeby@gmail.com>
2021-06-16 21:34:01 +0900

    quizzler -1

---
+++ 9_Quizzler/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "4 + 2 = 6",
+        "5 - 3 > 1",
+        "3 + 8 < 10"
+    ]
+    
+    var questionNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
+    
+    
+    
+}
+
